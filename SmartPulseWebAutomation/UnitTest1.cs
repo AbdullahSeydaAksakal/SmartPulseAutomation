7f4dc28c16cb9738d17c8c370bd585975a07dc34
Abdullah Seyda Aksakal <44892292+AbdullahSeydaAksakal@users.noreply.github.com>
2022-01-15 12:11:15 +0300

    WebTestCaseCreated

--- SmartPulseWebAutomation/UnitTest1.cs
+++ SmartPulseWebAutomation/UnitTest1.cs
@@ -1,12 +1,42 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
 using Xunit;
 
 namespace SmartPulseWebAutomation
 {
-    public class UnitTest1
+    public class KGUP
     {
-        [Fact]
-        public void Test1()
+
+        #region Defination
+        public IWebDriver? Chromedriver;
+        #endregion
+
+        #region Exit
+        internal void Dispose()
         {
+            Thread.Sleep(3000);
+            Chromedriver?.Quit();
+            Chromedriver?.Dispose();
+        }
+        #endregion
+
+        [Fact]
+        public void KGUPPage()
+        {
+
+
+            Chromedriver = new ChromeDriver();
+            Chromedriver.Navigate().GoToUrl("https://seffaflik.epias.com.tr/transparency/uretim/planlama/kgup.xhtml");
+            Chromedriver.Manage().Window.Maximize();
+            Chromedriver.FindElement(By.Id("j_idt226:date1_input")).SendKeys(Keys.Clear);
+            Chromedriver.FindElement(By.Id("j_idt226:date1_input")).SendKeys("30.03.2020");
+            Chromedriver.FindElement(By.Id("j_idt226:date2_input")).SendKeys(Keys.Clear);
+            Chromedriver.FindElement(By.Id("j_idt226:date2_input")).SendKeys("30.03.2020");
+            Chromedriver.FindElement(By.Id("j_idt226:goster")).Click();
+            Chromedriver.FindElement(By.Id("j_idt226:dt:j_idt276")).Click();
+            Dispose();
+
 
         }
     }
